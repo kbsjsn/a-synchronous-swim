e665297172cc35061c2070268ffae4bac1c8d9fe
kbsjsn <shin.kirk@gmail.com>
2019-06-15 15:08:56 -0700

    modified post request to include error message

--- client/js/keypressHandler.js
+++ client/js/keypressHandler.js
@@ -9,10 +9,9 @@ $('body').on('keydown', (event) => {
     $.ajax({
       type: 'POST',
       url: 'http://127.0.0.1:3000',
-      data: JSON.stringify(direction.toLowerCase()),
-      success: (data) => console.log(data)
-      //SwimTeam.move(data),
-      // complete: () => setTimeout(getSwimCommand, 1000)
+      data: direction.toLowerCase(),
+      success: (data) => console.log(`${data} command received`),
+      error: (data) => console.error(`Failed to send ${data}`)
     })
   }
 });
