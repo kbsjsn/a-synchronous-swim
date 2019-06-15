7d8bd5a2e63114590b2a739055a8b244615766c0
kbsjsn <shin.kirk@gmail.com>
2019-06-15 15:14:51 -0700

    refactored get request handler to accom. background image requests and text requests

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -1,38 +1,64 @@
-const fs = require('fs');
-const path = require('path');
-const headers = require('./cors');
-const multipart = require('./multipartUtils');
-const queue = require('../js/messageQueue');
-const keypressHandler = require('./keypressHandler');
+const fs = require("fs");
+const path = require("path");
+const headers = require("./cors");
+const multipart = require("./multipartUtils");
+const queue = require("../js/messageQueue");
+const keypressHandler = require("./keypressHandler");
 
 // Path for the background image ///////////////////////
-module.exports.backgroundImageFile = path.join('.', 'background.jpg');
+module.exports.backgroundImageFile = path.join(".", "background.jpg");
 ////////////////////////////////////////////////////////
 
 let messageQueue = null;
-module.exports.initialize = (queue) => {
-
+module.exports.initialize = queue => {
   messageQueue = queue;
-
 };
-
-module.exports.router = (req, res, next = ()=>{}) => {
-  console.log('Serving request type ' + req.method + ' for url ' + req.url);
-  if(req.method === 'GET') {
-    // keypressHandler.initialize(data => console.log("RESPONSE: ", data));
-    // let swimCommands = ['up', 'down', 'left', 'right'];
-    // let randomSwimIndex = Math.floor(Math.random() * swimCommands.length);
+console.log(module.exports.backgroundImageFile)
+module.exports.router = (req, res, next = () => {}) => {
+  console.log("Serving request type " + req.method + " for url " + req.url);
+  // if (req.method === "GET" && req.url === '/') {
+  //   // keypressHandler.initialize(data => console.log("RESPONSE: ", data));
+  //   // let swimCommands = ['up', 'down', 'left', 'right'];
+  //   // let randomSwimIndex = Math.floor(Math.random() * swimCommands.length);
+  //   // res.end(queue.dequeue());
+  //   res.writeHead(200, headers);
+  //   res.end(queue.dequeue())
     
-    res.writeHead(200, headers);
-    // res.end();
-    // console.log(res.end())
-    res.end(queue.dequeue());
+  // } else
+  if (req.url === '/background.jpg') {
+
+    
+    // // res.status(404)
+    // res.end()
+
+    console.log('Opening file')
+    fs.open(module.exports.backgroundImageFile, 'r', function(err, fd) {
+      if(err) {
+        res.writeHead(404);
+        //console.error(err)
+      } else { 
+        console.log('found');
+        res.writeHead(200, {'content-type':'image/jpeg'});
+        res.write('water-lg.multipart');
+        res.end('working!');
+      }
+ 
+    })
+
+    
   }
-
-  if(req.method === 'POST') {
-
-  }
-
+  if (req.method === "POST") {
   
-  next(); // invoke next() at the end of a request to help with testing!
+      req.on("error", () => {
+        console.error("error");
+      })
+      .on("data", chunk => {
+        queue.enqueue(chunk);
+      })
+      .on("end", () => {
+        req.end;
+      });
+
+    next(); // invoke next() at the end of a request to help with testing!
+  }
 };
